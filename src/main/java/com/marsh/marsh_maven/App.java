5bdbd0bd30852ed0f337e2a7a183cc46a370c530
amarjeet-arora <amarjeet.sn1@gmail.com>
2024-10-16 15:00:30 +0530

    updated

---
+++ src/main/java/com/marsh/marsh_maven/App.java
@@ -0,0 +1,13 @@
+package com.marsh.marsh_maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
